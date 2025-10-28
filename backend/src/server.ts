068eec797059f405a05ae497d5adb83c631215d2
xwang <xion36325@gmail.com>
2025-10-28 18:15:52 +0530

    initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+// import { type Request, type Response } from 'express';
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json()) 
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started on port http://localhost:${PORT}`)
+})
